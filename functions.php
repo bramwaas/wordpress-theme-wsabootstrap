c7710050096ce0513a14620dee8d9648134d87ca
bramwaas <bram@waasdorpsoekhan.nl>
2017-06-16 15:54:33 +0200

    Tot zover van teamtreehouse

--- functions.php
+++ functions.php
@@ -11,4 +11,11 @@ function wsabootstrap_scripts_with_jquery()
 }
 add_action( 'wp_enqueue_scripts', 'wsabootstrap_scripts_with_jquery' );
 
+if ( function_exists('register_sidebar') )
+	register_sidebar(array(
+			'before_widget' => '',
+			'after_widget' => '',
+			'before_title' => '<h3>',
+			'after_title' => '</h3>',
+	));
 ?>
